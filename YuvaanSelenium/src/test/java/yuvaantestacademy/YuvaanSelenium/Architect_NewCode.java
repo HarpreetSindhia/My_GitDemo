1e9f9b9d68f476b889c038129c139ddbb1ae69ba
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-06-25 14:24:33 +0530

    New Commit from Architect Guy

---
+++ YuvaanSelenium/src/test/java/yuvaantestacademy/YuvaanSelenium/Architect_NewCode.java
@@ -0,0 +1,20 @@
+package yuvaantestacademy.YuvaanSelenium;
+
+import org.testng.annotations.Test;
+
+public class Architect_NewCode {
+	
+	@Test
+	public void implementFramework() {
+		
+		System.out.println("Implementing the framework in Automation");
+	
+	}
+	
+	@Test
+	public void runAll() {
+		
+		System.out.println("Run all the test cases at night ");
+	}
+
+}
